71973ea58aae4cf50dbcf19e0937dc2d0ca446d1
Aleix Pol <aleixpol@kde.org>
2020-07-01 04:07:33 +0200

    Solve some clazy warnings
    My last patch triggered me to run clazy on the project. This patch
    solves some of them:
    - fixed qualification of return types. It's fine with doing it on return
    types as connects don't depend on it. It's relevant to QML though, see
    commit ac6bff575f934bceab03abdabc6a5c841c5b1f5e
    - Proper iteration of some types, saving their copy.
    - compile-time connects
    - prefer using connects with the 3rd argument as an object when using
    lambdas.

--- src/declarativeimports/core/tooltipdialog.cpp
+++ src/declarativeimports/core/tooltipdialog.cpp
@@ -39,7 +39,7 @@ ToolTipDialog::ToolTipDialog(QQuickItem  *parent)
 
     m_showTimer = new QTimer(this);
     m_showTimer->setSingleShot(true);
-    connect(m_showTimer, &QTimer::timeout, [ = ]() {
+    connect(m_showTimer, &QTimer::timeout, this, [this]() {
         setVisible(false);
     });
 }
