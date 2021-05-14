KDE/plasma-framework

commit a45bd758001310d39b9c36ee1617cc12d622c62a

Fix elide of TabButtons
Without fillWidth: true the width is unmanaged by the GridView

If the width is unmanaged it will fit the text

BUG: 434945

src/declarativeimports/plasmacomponents3/TabButton.qml
@@ -60,6 +60,7 @@ T.TabButton {
         }
         Label {
             id: label
+            Layout.fillWidth: true
             Layout.alignment: control.display != T.AbstractButton.TextBesideIcon || icon.source.length === 0 ? Qt.AlignCenter : Qt.AlignVCenter | Qt.AlignLeft
             visible: text.length > 0 && control.display !== T.AbstractButton.IconOnly
             text: control.Kirigami.MnemonicData.richTextLabel

Sonic-DE/sonic-interface-libraries

commit 532cc8c4625d6d84ccc881d31596f34711312986

PC3 TabButton: Fix horizontal content alignment with max text width
bug was caused by commit a45bd758001310d39b9c36ee1617cc12d

src/declarativeimports/plasmacomponents3/TabButton.qml
@@ -61,6 +61,7 @@ T.TabButton {
         Label {
             id: label
             Layout.fillWidth: true
+            Layout.maximumWidth: Math.ceil(implicitWidth)
             visible: text.length > 0 && control.display !== T.AbstractButton.IconOnly
             text: control.Kirigami.MnemonicData.richTextLabel
             font: control.font