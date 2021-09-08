6050d952484934d6b48e4102f17f046c6ed7a64a
Aleix Pol <aleixpol@kde.org>
2021-09-08 16:23:51 +0200

    Do not compute sizes within dialogs when they're not visible
    It makes our system do stuff unnecessarily, it often warns that the
    dialog is not even the right size and it will be called anyway when the
    dialog is displayed, so just do it when it's necessary.

--- src/plasmaquick/dialog.cpp
+++ src/plasmaquick/dialog.cpp
@@ -580,7 +580,7 @@ void DialogPrivate::syncToMainItemSize()
 {
     Q_ASSERT(mainItem);
 
-    if (!componentComplete) {
+    if (!componentComplete || !q->isExposed()) {
         return;
     }
     if (mainItem->width() <= 0 || mainItem->height() <= 0) {
@@ -1124,7 +1124,7 @@ void Dialog::resizeEvent(QResizeEvent *re)
     }
 
     // A dialog can be resized even if no mainItem has ever been set
-    if (!d->mainItem) {
+    if (!d->mainItem || !isExposed()) {
         return;
     }
 
