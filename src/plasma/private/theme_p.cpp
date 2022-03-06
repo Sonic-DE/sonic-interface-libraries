9b5ab6a66c57a48ec5e6b8b9153b90971d315dcc
Alexander Lohnau <alexander.lohnau@gmx.de>
2022-03-06 10:03:53 +0100

    Plasma::Theme: Port last KPluginInfo usage

--- src/plasma/private/theme_p.cpp
+++ src/plasma/private/theme_p.cpp
@@ -59,7 +59,10 @@ KPluginMetaData getMetaDataForTheme(const QString &theme)
     }
 #if KCOREADDONS_BUILD_DEPRECATED_SINCE(5, 92)
     else if (QFileInfo::exists(packageBasePath + QLatin1String("/metadata.desktop"))) {
+    QT_WARNING_PUSH
+    QT_WARNING_DISABLE_DEPRECATED
         return KPluginMetaData::fromDesktopFile(packageBasePath + QLatin1String("/metadata.desktop"));
+    QT_WARNING_POP
     }
 #endif
     else {
@@ -651,8 +654,8 @@ void ThemePrivate::settingsFileChanged(const QString &file)
 {
     qCDebug(LOG_PLASMA) << "settingsFile: " << file;
     if (file == themeMetadataPath) {
-        const KPluginInfo pluginInfo(themeMetadataPath);
-        if (!pluginInfo.isValid() || themeVersion != pluginInfo.version()) {
+        const KPluginMetaData data = getMetaDataForTheme(themeName);
+        if (!data.isValid() || themeVersion != data.version()) {
             scheduleThemeChangeNotification(SvgElementsCache);
         }
     } else if (file.endsWith(QLatin1String(themeRcFile))) {
