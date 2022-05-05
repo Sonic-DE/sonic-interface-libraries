7837d7bea82cc1b8ea6e247349fa8dcf9b14870b
Niccolò Venerandi <niccolo@venerandi.com>
2022-05-05 17:51:39 +0200

    Do not scale framesvgitem if we are currently using qt scaling BUG: 438644

--- src/declarativeimports/core/framesvgitem.cpp
+++ src/declarativeimports/core/framesvgitem.cpp
@@ -684,7 +684,12 @@ void FrameSvgItem::componentComplete()
 
 void FrameSvgItem::updateDevicePixelRatio()
 {
-    m_frameSvg->setScaleFactor(qMax<qreal>(1.0, floor(Units::instance().devicePixelRatio())));
+    if (qEnvironmentVariableIsSet("PLASMA_USE_QT_SCALING")) {
+        m_frameSvg->setScaleFactor(1.0);
+    } else {
+        m_frameSvg->setScaleFactor(qMax<qreal>(1.0, floor(Units::instance().devicePixelRatio())));
+    }
+
 
     // devicepixelratio is always set integer in the svg, so needs at least 192dpi to double up.
     //(it needs to be integer to have lines contained inside a svg piece to keep being pixel aligned)
