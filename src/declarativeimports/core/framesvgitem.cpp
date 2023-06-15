3163fce6b4fea2f5ba2ffac4b837549b5d8f9cb5
Marco Martin <notmart@gmail.com>
2023-06-15 15:52:25 +0200

    remove warning messages
    not useful as from there i can't know the file it comes from

--- src/declarativeimports/core/framesvgitem.cpp
+++ src/declarativeimports/core/framesvgitem.cpp
@@ -283,7 +283,6 @@ FrameSvgItem::FrameSvgItem(QQuickItem *parent)
     , m_sizeChanged(false)
     , m_fastPath(true)
 {
-    qWarning() << "PlasmaCore.FrameSvgItem is deprecated: use KSvg.FrameSvgItem instead.";
     m_frameSvg = new Plasma::FrameSvg(this);
     setFlag(ItemHasContents, true);
     connect(m_frameSvg, &FrameSvg::repaintNeeded, this, &FrameSvgItem::doUpdate);
