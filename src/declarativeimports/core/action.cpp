7ddfe82b0342cfdd8bbf11182b7b10ccc6780bac
Marco Martin <notmart@gmail.com>
2023-06-26 18:10:17 +0200

    QQmlListProperty qmlContextualActions
    also, more properties in the qaction extender

--- src/declarativeimports/core/action.cpp
+++ src/declarativeimports/core/action.cpp
@@ -6,9 +6,30 @@
 
 #include "action.h"
 
-IconGroup::IconGroup(QObject *parent)
+IconGroup::IconGroup(ActionExtension *parent)
     : QObject(parent)
+    , m_action(parent->action())
 {
+}
+
+IconGroup::~IconGroup()
+{
+}
+
+void IconGroup::setName(const QString &name)
+{
+    if (name == m_name) {
+        return;
+    }
+
+    m_name = name;
+    m_action->setIcon(QIcon::fromTheme(name));
+    Q_EMIT nameChanged();
+}
+
+QString IconGroup::name() const
+{
+    return m_name;
 }
 
 ActionExtension::ActionExtension(QObject *parent)
@@ -20,6 +41,42 @@ ActionExtension::ActionExtension(QObject *parent)
 
 ActionExtension::~ActionExtension()
 {
+}
+
+bool ActionExtension::isSeparator() const
+{
+    return m_action->isSeparator();
+}
+
+void ActionExtension::setSeparator(bool separator)
+{
+    if (separator == m_action->isSeparator()) {
+        return;
+    }
+
+    m_action->setSeparator(separator);
+
+    Q_EMIT isSeparatorChanged();
+}
+
+void ActionExtension::setActionGroup(QActionGroup *group)
+{
+    if (group == m_action->actionGroup()) {
+        return;
+    }
+
+    m_action->setActionGroup(group);
+    Q_EMIT actionGroupChanged();
+}
+
+QActionGroup *ActionExtension::actionGroup() const
+{
+    return m_action->actionGroup();
+}
+
+QAction *ActionExtension::action() const
+{
+    return m_action;
 }
 
 #include "moc_action.cpp"
