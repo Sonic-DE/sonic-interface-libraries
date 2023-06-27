1f8f49b18ac3fde06bd47b6540c76da8a89981bd
Marco Martin <notmart@gmail.com>
2023-06-27 16:36:16 +0200

    Full api
    pieces of old api survive as set/removeInternalAction
    everything else is QQMlListProperty based.
    C++ Applets that want to add actions, will have to expose a new
    action property which will be assigned to the listproperty on qml side
    
    QML support for shortcuts

--- src/plasmaquick/plasmoid/appletinterface.cpp
+++ src/plasmaquick/plasmoid/appletinterface.cpp
@@ -52,7 +52,9 @@ void AppletInterface::init()
 
     auto *applet = AppletInterface::applet();
     auto connectActions = [this, applet]() {
+        qWarning() << applet->contextualActions().count();
         for (auto *a : applet->contextualActions()) {
+            // TODO: when the old api goes, whole executeAction logic will be deleted
             if (!m_actions.contains(a)) {
                 connect(a, &QAction::triggered, this, [this, a]() {
                     executeAction(a);
