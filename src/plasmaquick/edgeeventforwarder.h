7d915d660ea5fecfcc8affb484c193abcb3ac505
David Edmundson <kde@davidedmundson.co.uk>
2023-08-31 12:22:18 +0100

    squash with event forwarder

--- src/plasmaquick/edgeeventforwarder.h
+++ src/plasmaquick/edgeeventforwarder.h
@@ -10,25 +10,36 @@ class QWindow;
 
 class EdgeEventForwarderPrivate;
 
+namespace PlasmaQuick
+{
+
 /**
  * @brief The EdgeEventForwarder class
  * This class forwards edge events to be replayed within the given margin
  * This is useful if children do not touch the edge of a window, but want to get input events
  */
-
-namespace PlasmaQuick
-{
-
 class PLASMAQUICK_EXPORT EdgeEventForwarder : public QObject
 {
     Q_OBJECT
 public:
+    /**
+     * @brief EdgeEventForwarder constructor
+     * @param window The window to intercept and filter
+     * The event forwarder is parented to the window
+     */
     EdgeEventForwarder(QWindow *parent);
     ~EdgeEventForwarder();
 
+    /**
+     * @brief setMargins sets the margins to use for the event forwarding
+     */
     void setMargins(const QMargins &margins);
     QMargins margins();
 
+    /**
+     * @brief setActiveEdges sets which margins should be active for edge forwarding
+     * typically this should match edges touching a screen edge
+     */
     void setActiveEdges(Qt::Edges edges);
     Qt::Edges activeEdges();
 
