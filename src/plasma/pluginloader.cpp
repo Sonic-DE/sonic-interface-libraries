712f369e422b81f2c85f14a4e00ff797206486f1
Nicolas Fella <nicolas.fella@gmx.de>
2023-09-18 21:57:42 +0200

    [pluginloader] Remove parent app handling from listAppletMetaDataForUrl
    It's unused
    
    This makes it consistent with listAppletMetaData

--- src/plasma/pluginloader.cpp
+++ src/plasma/pluginloader.cpp
@@ -237,16 +237,9 @@ QList<KPluginMetaData> PluginLoader::listAppletMetaDataForMimeType(const QString
 
 QList<KPluginMetaData> PluginLoader::listAppletMetaDataForUrl(const QUrl &url)
 {
-    QString parentApp;
-    QCoreApplication *app = QCoreApplication::instance();
-    if (app) {
-        parentApp = app->applicationName();
-    }
-
-    auto filter = [&parentApp](const KPluginMetaData &md) -> bool {
+    auto filter = [](const KPluginMetaData &md) -> bool {
         const QString pa = md.value(QStringLiteral("X-KDE-ParentApp"));
-        return (parentApp.isEmpty() || pa == parentApp) //
-            && !md.value(QStringLiteral("X-Plasma-DropUrlPatterns"), QStringList()).isEmpty();
+        return !md.value(QStringLiteral("X-Plasma-DropUrlPatterns"), QStringList()).isEmpty();
     };
     const QList<KPluginMetaData> allApplets = KPackage::PackageLoader::self()->findPackages(QStringLiteral("Plasma/Applet"), QString(), filter);
 
