f7c6bb84df5634850538a6a5959cd048d1f317c4
Marco Martin <notmart@gmail.com>
2023-12-22 16:25:24 +0100

    also implicitsize

--- src/plasmaquick/appletpopup.cpp
+++ src/plasmaquick/appletpopup.cpp
@@ -280,11 +280,11 @@ QSize LayoutChangedProxy::implicitSize() const
         size = QSize(m_item->implicitWidth(), m_item->implicitHeight());
     }
     qreal width = m_preferredWidth.read().toReal();
-    if (qIsFinite(width) && width > 0) {
+    if (qIsFinite(width) && int(width) > 0) {
         size.setWidth(width);
     }
     qreal height = m_preferredHeight.read().toReal();
-    if (qIsFinite(height) && height > 0) {
+    if (qIsFinite(height) && int(height) > 0) {
         size.setHeight(height);
     }
     return size;
