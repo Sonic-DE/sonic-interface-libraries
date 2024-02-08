6b62d436482931476d23389c5be1f23b4036b17b
Marco Martin <notmart@gmail.com>
2024-02-08 16:57:13 +0100

    Guard against invalid URL mimedata with no urls
    It's possible that the dropped mimedata claims to have urls, but its
    actual url list is empty. This happens for instance when droppping
    the url of chromium from its addressbar.
    
    Guard against it to prevent an assert
    
    CCBUG:480312

--- src/plasmaquick/plasmoid/containmentitem.cpp
+++ src/plasmaquick/plasmoid/containmentitem.cpp
@@ -385,7 +385,7 @@ void ContainmentItem::processMimeData(QMimeData *mimeData, int x, int y, KIO::Dr
                                        Q_ARG(QRectF, QRectF(x, y, -1, -1)));
         }
         delete m_dropMenu.data();
-    } else if (mimeData->hasUrls()) {
+    } else if (mimeData->hasUrls() && !mimeData->urls().isEmpty()) {
         // TODO: collect the mimetypes of available script engines and offer
         //      to create widgets out of the matching URLs, if any
         const QList<QUrl> urls = KUrlMimeData::urlsFromMimeData(mimeData);
