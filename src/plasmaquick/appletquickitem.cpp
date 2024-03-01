404f7b0951780838116c26cfee4bb9580ab716de
Nate Graham <nate@kde.org>
2024-03-01 11:02:29 +0000

    Apply 3 suggestion(s) to 1 file(s)

--- src/plasmaquick/appletquickitem.cpp
+++ src/plasmaquick/appletquickitem.cpp
@@ -544,20 +544,20 @@ AppletQuickItem *AppletQuickItem::itemForApplet(Plasma::Applet *applet)
                 "This Widget was written for an unknown older version of Plasma and is not compatible with Plasma %1. Please contact the widget's author for "
                 "an updated version.",
                 plasma_version_major);
-            compactReason = i18n("This Widget is not compatible with Plasma %1", plasma_version_major);
+            compactReason = i18n("%1 is not compatible with Plasma %2", applet->pluginMetaData().name(), plasma_version_major);
             versionMismatch = true;
         } else if (version.majorVersion() < plasma_version_major) {
             reason =
                 i18n("This Widget was written for Plasma %1 and is not compatible with Plasma %2. Please contact the widget's author for an updated version.",
                      version.majorVersion(),
                      plasma_version_major);
-            compactReason = i18n("This Widget is not compatible with Plasma %1", plasma_version_major);
+            compactReason = i18n("%1 is not compatible with Plasma %2", applet->pluginMetaData().name(), plasma_version_major);
             versionMismatch = true;
         } else if (version.majorVersion() > plasma_version_major || version.minorVersion() > PLASMA_VERSION_MINOR) {
             reason = i18n("This Widget was written for Plasma %1 and is not compatible with Plasma %2. Please update Plasma in order to use the widget.",
                           versionString,
                           plasma_version_major);
-            compactReason = i18n("This Widget is not compatible with Plasma %1", plasma_version_major);
+            compactReason = i18n("%1 is not compatible with Plasma %2", applet->pluginMetaData().name(), plasma_version_major);
             versionMismatch = true;
         } else if (applet->failedToLaunch()) {
             reason = applet->launchErrorMessage();
