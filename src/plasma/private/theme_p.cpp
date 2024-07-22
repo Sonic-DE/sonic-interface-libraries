b1f5e25f190a8df96a6a54fa6f4ac6c31ac62af0
Niccolo Venerandi <niccolo@venerandi.com>
2024-07-22 12:31:13 +0200

    Make Breeze Dark and Light plasma themes follow system accent color

--- src/plasma/private/theme_p.cpp
+++ src/plasma/private/theme_p.cpp
@@ -363,8 +363,12 @@ void ThemePrivate::colorsChanged()
 {
     // in the case the theme follows the desktop settings, refetch the colorschemes
     // and discard the svg pixmap cache
-    if (!colors) {
-        KSharedConfig::openConfig()->reparseConfiguration();
+    if (colors != nullptr) { // one less file read
+        // By loading another config, the prior config's cache seems to get discarded.
+        // FIXME: Find a less hacky way to allow the colours filling in missing parts
+        // of partial Plasma Style palettes to be refreshed when changing colour schemes
+        colors = KSharedConfig::openConfig(QString(), KConfig::SimpleConfig);
+        colors = KSharedConfig::openConfig(colorsFile);
     }
     colorScheme = KColorScheme(QPalette::Active, KColorScheme::Window, colors);
     buttonColorScheme = KColorScheme(QPalette::Active, KColorScheme::Button, colors);
@@ -595,10 +599,9 @@ void ThemePrivate::setThemeName(const QString &tempThemeName, bool writeSettings
     themeName = theme;
 
     // load the color scheme config
-    const QString colorsFile = realTheme
-        ? QStandardPaths::locate(QStandardPaths::GenericDataLocation,
-                                 QLatin1String(PLASMA_RELATIVE_DATA_INSTALL_DIR "/desktoptheme/") % theme % QLatin1String("/colors"))
-        : QString();
+    colorsFile = realTheme ? QStandardPaths::locate(QStandardPaths::GenericDataLocation,
+                                                    QLatin1String(PLASMA_RELATIVE_DATA_INSTALL_DIR "/desktoptheme/") % theme % QLatin1String("/colors"))
+                           : QString();
 
     // qCDebug(LOG_PLASMA) << "we're going for..." << colorsFile << "*******************";
 
