1342bb14a7728628ef6cfc53b878799a8f19c2cf
Marco Martin <notmart@gmail.com>
2025-05-23 17:41:17 +0200

    Fix drag and drop of item on containments
    ContainmentItem::createApplet wants a position relative to the containment,
    while sendng dropPoint() is an absolute screen coordinate position
    
    Drop will be broken for centered panels and containments in screens not
    at 0,0 position.
    transform the coordinates to be relative to the contaiment
    
    BUG:460056

--- src/plasmaquick/plasmoid/dropmenu.h
+++ src/plasmaquick/plasmoid/dropmenu.h
@@ -27,11 +27,11 @@ class DropMenu : public QObject
     Q_OBJECT
 
 public:
-    DropMenu(KIO::DropJob *dropJob, const QPoint &dropPoint, ContainmentItem *parent = nullptr);
+    DropMenu(KIO::DropJob *dropJob, const QPoint &screenDropPoint, ContainmentItem *parent = nullptr);
     ~DropMenu() override;
 
     QList<QUrl> urls() const;
-    QPoint dropPoint() const;
+    QPoint screenDropPoint() const;
     void setUrls(const QList<QUrl> &urls);
     void setMultipleMimetypes(bool multipleMimetypes);
 
@@ -41,7 +41,7 @@ public:
     void show();
 
 private:
-    QPoint m_dropPoint;
+    QPoint m_screenDropPoint;
     QMenu *m_menu = nullptr;
     KIO::DropJob *m_dropJob = nullptr;
     QList<QAction *> m_dropActions = QList<QAction *>();
