12e9ba05e3e954352202205d622e50860d219e99
Nicolas Fella <nicolas.fella@gmx.de>
2025-07-27 19:53:16 +0200

    [pluginloader] Fix loading non-KPackage-based containments

--- autotests/applet/applettest.cpp
+++ autotests/applet/applettest.cpp
@@ -87,6 +87,13 @@ private Q_SLOTS:
     void testListContainment()
     {
         auto containments = Plasma::PluginLoader::listContainmentsMetaData();
+        QVERIFY(!containments.isEmpty());
+
+        auto testContainment = std::find_if(containments.cbegin(), containments.cend(), [](const KPluginMetaData &md) {
+            return md.pluginId() == "org.kde.plasma.testcontainment";
+        });
+
+        QVERIFY(testContainment != containments.cend());
     }
 };
 
